fd0aaa17bc526ae7e439cd8d3ed02972903a511e
pgl <1801688332@qq.com>
2021-09-03 10:05:13 +0800

    feat:初始化配置axios

--- src/global/index.ts
+++ src/global/index.ts
@@ -1,10 +1,10 @@
 // import 'element-plus/lib/theme-chalk/base.css'
 import 'element-plus/dist/index.css'
 import { ElButton } from 'element-plus'
-import { App } from '@vue/runtime-core'
+import { App } from 'vue'
 
 const components = [ElButton]
-export function register(app: App<Element>) {
+export function register(app: App) {
   for (const component of components) {
     app.component(component.name, component)
   }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { register } from './global'
+import './service/axios'
 
 const app = createApp(App)
 register(app)
