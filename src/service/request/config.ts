79a0e83053b460044592fa320f210e2f67030640
pgl <1801688332@qq.com>
2021-09-05 21:48:29 +0800

    feat:配置开发环境变量

---
+++ src/service/request/config.ts
@@ -0,0 +1,15 @@
+// let BASE_URL = ''
+// let BASE_NAME = ''
+
+// if (process.env.NODE_ENV === 'developement') {
+//   BASE_URL = 'http://www.baidu.com/dev'
+//   BASE_NAME = 'pgl'
+// } else if (process.env.NODE_ENV === 'production') {
+//   BASE_URL = 'http://www.baidu.com/pro'
+//   BASE_NAME = 'pgl'
+// } else {
+//   BASE_URL = 'http://www.baidu.com/test'
+//   BASE_NAME = 'pgl'
+// }
+
+// export { BASE_URL, BASE_URL }
--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,13 @@ const path = require('path')
 
 module.exports = {
   outputDir: './build',
-  // webpack属性会合并
-  configureWebpack: {
-    resolve: {
-      alias: {
-        components: '@/components'
-      }
-    }
-  }
+
+  // // webpack属性会合并
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       components: '@/components'
+  //     }
+  //   }
+  // }
 }
